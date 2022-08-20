300305ba7b481992ca333f28cb5a2f690df1e0f5
Gutha Bharath Kumar <38840613+gbharatnaidu@users.noreply.github.com>
2022-08-20 13:42:46 +0530

    Update circular_queue_arrays.c

--- circular_queue_arrays.c
+++ circular_queue_arrays.c
@@ -1,68 +1,76 @@
-#include<stdio.h>
-#define MAX 3
-int queue[MAX];
-int front=-1,rear=-1;
-void enqueue(){
-    int data;
-    printf("enter the data to insert\n");
-    scanf("%d",&data);
-    if(front==-1)
-        front=rear=0;
-    else if( (rear+1)%MAX == front){
-        printf("overflow condition in queue\n");
+
+#include <stdio.h>
+#include <stdlib.h>
+#define max 5
+int queue[max];
+int head=-1,tail=-1;
+int enqueue(int val)
+{
+    
+    if((tail+1)%max == head)
+    {
+        printf("queue overflow\n");
     }
     else
-        rear=(rear+1)%MAX;
-    queue[rear]=data;
+    {
+        if(head==-1)
+            head=tail=0;
+        else
+            tail=(tail+1)%max;
+        queue[tail]=val;
+    }
 }
 
-void dequeue(){
-    if(front==-1)
-        printf("underflow\n");
-    else if(front==rear)
-        front=rear=-1;
+int dequeue()
+{
+    if(head==-1)
+        printf("undeflow");
     else
-        front=(front+1)%MAX;
+    {
+        if(head==tail)
+            head=tail=-1;
+        else
+            head=(head+1)%max;
+    }
 }
-
-void display(){
-    if(front==-1)
-        printf("no elements in queue\n");
-   /* else{
-        if(rear>=front){
-            for(int i=front;i<=rear;i++)
-            printf("%d ",queue[i]);
-        }
-        else{
-            for(int i=front;i<MAX;i++)
-            printf("%d ",queue[i]);
-            for(int i=0;i<=rear;i++)
-            printf("%d ",queue[i]);
-        }
-    }*/
-    for(int i=front;i<=rear;i=(i+1)%MAX){
+void display()
+{
+    int i=head;
+    while(i!=tail)
+    {
         printf("%d ",queue[i]);
+        i= (i+1)%max;
     }
+    printf("%d ",queue[i]);
     printf("\n");
 }
-void peek(){
-    printf("the first element in the queue is : %d\n",queue[front]);
-}
-int main(){
-    while(1){
-    printf("1.enqueue\n2.dequeue\n3.display\n4.peek\n5.exit\n");
-    int n;
-    scanf("%d",&n);
-        switch(n){
-            case 1:enqueue();
-            break;
-            case 2:dequeue();
-            break;
-            case 3:display();
-            break;
-            case 4:peek();
-            break;
-            case 5:exit(1);
+int main()
+{
+    
+    while(1)
+    {
+        printf("choose the option\n1.enqueue\n2.dequeue\n3.display\n4.exit\n");
+        int n,val;
+        scanf("%d",&n);
+        switch(n)
+        {
+            case 1:
+                printf("enter the value\n");
+                scanf("%d",&val);
+                enqueue(val);
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                exit(1);
+                break;
         }
     }
+    
+
+    return 0;
 }
